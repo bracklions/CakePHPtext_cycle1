0ad399a34a6ea890480fbec53d8dea6a610cf811
bracklions <deerlions@yahoo.co.jp>
2019-06-03 21:03:05 +0900

    initial commit

--- src/Controller/PeopleController.php
+++ src/Controller/PeopleController.php
@@ -5,7 +5,45 @@ use App\Controller\AppController;
 
 class PeopleController extends AppController {
     public function index() {
-        $data = $this->People->find('all');
+        if ($this->request->is('post')) {
+            $find = $this->request->data['People']['find'];
+            // $arr = explode(',',$find);
+            // $condition = ['conditions'=>['name like'=>$find]];
+            // $condition = ['conditions'=>['age <=' => $find]];
+            // $condition = ['conditions'=>[
+                // 'and' =>[
+                //     'age >=' => $arr[0],
+                //     'age <=' => $arr[1]
+                // 'or' =>[
+                //     'name like' => $find,
+                //     'mail like' => $find
+                // ]
+            // ]];
+            // $condition = [
+            //     'conditions' => ['name like' => $find],
+            //     'order' => ['People.age' => 'desc'] 
+            // ];
+            // $condition = ['limit' => 3, 'page' => $find];
+            // $data = $this->People->find('all', $condition);
+            // $data = $this->People->findByNameOrMail($find, $find);
+            // $data = $this->People->find()->where(['name'=>$find]);
+            // $data = $this->People->find()
+                // ->where(['age >=' => $arr[0]])
+                // ->andwhere(['age <=' => $arr[1]])
+                // ->order(['People.age' => 'asc']);
+                // ->order(['People.age' => 'asc'])
+                // ->order(['People.name' => 'asc'])
+                // ->limit(3)->page($find);
+            $data = $this->People->find('me', ['me' => $find]);
+        } else {
+            // $data = $this->People->find('all');
+            // $data = $this->People->find('all',
+            //     ['order' => ['People.age' => 'asc']]
+            // $data = $this->People->find()
+            //     ->order(['People.age' => 'asc'])
+            //     ->order(['People.name' => 'asc']);
+            $data = $this->People->find('byAge');
+        }
         $this->set('data', $data);
     }
 
@@ -41,10 +79,21 @@ class PeopleController extends AppController {
     }
 
     public function add() {
+        $msg = 'please type your personal data...';
         $entity = $this->People->newEntity();
+        if ($this->request->is('post')){
+            $data = $this->request->data['People'];
+            $entity = $this->People->newEntity($data);
+            if ($this->People->save($entity)) {
+                return $this->redirect(['actions'=>'index']);
+            }
+            $msg = 'Error was occured...';
+        }
+        $this->set('msg', $msg);
         $this->set('entity', $entity);
     }
 
+    // addを追加したら使わなくなった
     public function create() {
         if ($this->request->is('post')) {
             $data = $this->request->data['People'];
