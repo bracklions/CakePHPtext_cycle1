4e21d67fdbd696e5eb6ee53e354c0efa6c8ca59d
bracklions <deerlions@yahoo.co.jp>
2019-06-11 23:33:16 +0900

    6-2 認証コンポーネントを使おう
    完了

--- src/Controller/UsersController.php
+++ src/Controller/UsersController.php
@@ -66,7 +66,8 @@ class UsersController extends AppController
     // 認証を使わないページの設定
     public function beforeFilter(Event $event) {
         parent::beforeFilter($event);
-        $this->Auth->allow(['login', 'index', 'add']);// あとで'add'を削除する
+        //$this->Auth->allow(['login', 'index', 'add']);// あとで'index'とadd'を削除する
+        $this->Auth->allow(['login']);
     }
 
     // 認証時のロールのチェック
